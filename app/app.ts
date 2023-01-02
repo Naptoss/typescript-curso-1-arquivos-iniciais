751ab06924eba82246174bdf94a72694df27209b
Naptoss <tomgabinio@gmail.com>
2023-01-02 17:30:21 -0300

    master

---
+++ app/app.ts
@@ -0,0 +1,4 @@
+import { Negociacao } from "./controllers/negociacao";
+
+const negociacao = new Negociacao(new Date(), 10, 100)
+console.log(negociacao.volume)
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,3 @@
-alert('oi')
+import { Negociacao } from './controllers/negociacao.js';
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao.volume);
